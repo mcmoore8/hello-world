a2eca0932673e055e7fb9089fd6dadef86ac63bc
mcmoore8 <79734936+mcmoore8@users.noreply.github.com>
2021-02-26 21:39:20 -0700

    Commit to First Branch(abc)
    Changes made include:
    Add name(line 2)
    Add previousNumber function(line 17)
    Add reverseNum variable(line6)
    Add reverseNum function(line 46)

--- GitDemo.java
+++ GitDemo.java
@@ -1,8 +1,9 @@
 /*****
- * Edit this line to have your name.
+ * Michael Moore
  ******/
 public class GitDemo {
     
+    private double reverseNum;
     private double previousNumber;
     
     public GitDemo() {
@@ -13,6 +14,7 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
+        previousNumber = previousNumber + num;
         System.out.print("Add num to previousNumber");
         return 0;
     }
@@ -41,6 +43,7 @@ public class GitDemo {
      *****/
     public double reverseNumber(int num){
         double newNumber = 1;
+        reverseNum = newNumber;
         
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
